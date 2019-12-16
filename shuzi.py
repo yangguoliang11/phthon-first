975e1a262441a37e6592323bf2ae01a36a11c87c
guoliang <yanggl@jvtd.cn>
2019-12-16 20:16:39 +0800

    所有的py的demo

---
+++ shuzi.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+print 45678 + 0x12fd2
+print 100<99
+print None
+print True
+print False
+print "aa","bb","cc","dd"
+x1 = 1
+d = 3
+n = 100
+
+print r'''aa
+bb
+cc
+dd
+ff'''
+
+print r'''"To be, or not to be": that is the question.
+Whether it's nobler in the mind to suffer.'''
+print u'中文'
