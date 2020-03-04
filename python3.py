0ebc9241894ca6d06978793cba754e68a95e10fb
guoliang <yanggl@jvtd.cn>
2020-03-04 18:11:30 +0800

    python3学习

---
+++ python3.py
@@ -0,0 +1,40 @@
+# -*- coding: UTF-8 -*-
+import sys
+from sys import argv,path
+
+print ("Hello Python!")
+
+if True:
+    print ("True")
+else:
+    print ("False")
+
+test1 = 1
+test2 = 2
+test3 = 3
+
+test = test1 + \
+       test2 + \
+       test3
+print (test)
+
+
+test4 = ['tem','te33',
+        'test3355']
+
+print(test4)
+
+num1 = 1
+num2 = True
+num3 = 1.23
+num4 = 1.1+2.2j
+
+str1 = "abcde"
+print(str1[2:5:2])
+
+print(sys.api_version)
+
+print(str1*3)
+
+
+
